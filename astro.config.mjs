17b2932dd547fd4c33280308c630b50468ef0bfa
Pedro Sampaio <pedro@codvoc.com>
2025-06-16 21:39:27 +0100

    Use server rendering for keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import mdx from "@astrojs/mdx";
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
       plugins: [tailwindcss()]
   },
