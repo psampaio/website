f7d2ffd70646357c6297cb28ad459485b30b4822
Pedro Sampaio <pedro@codvoc.com>
2025-06-16 21:27:34 +0100

    Added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,18 @@ import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import mdx from "@astrojs/mdx";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react(), keystatic(), mdx()],
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [react(), keystatic(), mdx()],
+
+  adapter: node({
+    mode: "standalone",
+    
+  }),
 });
